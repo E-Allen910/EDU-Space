4264f1161a1356f297ecb5fbd37d0e6bc629730d
unknown <esau.allen45@gmail.com>
2021-08-18 10:28:51 -0400

    added the message board HTML

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let nickName = document.querySelector("#username");
 let inputText = document.querySelector("#message");
-let button = document.querySelector("#submitButton");
-let mainContainer = document.querySelector(".allText");
+let timeText = document.querySelector("#time");
+let button = document.querySelector("#submiting");
+let mainBoard = document.querySelector(".allText")
+/* This will be the variable used for the main message board*/
+/*let mainContainer = document.querySelector(".allText");*/
 
 let database = firebase.database().ref();
 
@@ -9,14 +12,17 @@ button.onclick = function updateDB(event) {
     event.preventDefault();
     let username = nickName.value;
     let message = inputText.value;
+    let time = inputText.value;
 
     nickName.value = "";
     inputText.value = "";
-    console.log(username + " : " + message);
+    console.log(username + " : " + time + " : " + message);
 
     let value = {
         NAME: username,
+        TIME: time,
         MESSAGE: message,
+    
     } 
 
     database.push(value);
@@ -27,8 +33,9 @@ database.on("child_added", addMessageToBoard);
 function addMessageToBoard(rowData) {
     let row = rowData.val(); //retuns an object just like the "value" we pushed
     let username = row.NAME;
+    let time = row.TIME;
     let message = row.MESSAGE;
     let p = document.createElement("p");
-    p.innerHTML = username + ": " + message;
-    mainContainer.appendChild(p);
+    p.innerHTML = username + ": " + time + " : " + message;
+    userBoard.appendChild(p);
 } 
